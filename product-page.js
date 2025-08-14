03a14f01f62266b722a8610a4edc9dddfc3b5dbb
Andone0912 <andonedarius04@gmail.com>
2025-08-15 00:35:51 +0300

    added another animation, made it bug free for the responsive layout

--- product-page.js
+++ product-page.js
@@ -4,11 +4,18 @@ const minusButton = document.querySelector(".minus-button");
 const toggleButton = document.querySelector(".hidden-toggle-btn")
 const plusIcon = document.querySelector(".plus-icon")
 const minusIcon = document.querySelector(".minus-icon")
+const verticalBar = document.querySelector(".vertical-bar")
+const mm = gsap.matchMedia();
+
+document.querySelector(".page-two").style.position = "absolute";
+document.querySelector(".page-two").style.top = "0px";
 
 plusButton.addEventListener("click", expandFunction);
 minusButton.addEventListener("click", minimizeFunction);
 toggleButton.addEventListener("click", toggleInformation);
+
 let isOpen = false;
+let pageTwo = false;
 
 function expandFunction() {
   if (isOpen) return;
@@ -73,6 +80,80 @@ function toggleInformation() {
 }
 
 
+mm.add("(min-width: 640px)", () => {
+  console.log("Desktop listener added");
+  document.querySelector(".page-two").style.removeProperty("transform");
+  document.querySelector(".page-one").style.removeProperty("transform");
+
+
+
+  const verticalBar = document.querySelector(".vertical-bar");
+
+  function scrollHeroDesktop() {
+    if (pageTwo) {
+      gsap.to(".vertical-bar > div", { y: 0, duration: 0.5, ease: "power2.inOut" });
+      gsap.fromTo(".page-one",
+         { y: -200, x:0, opacity: 0},
+         { y: 0, x:0, opacity: 1, duration: 0.9, ease: "power2.inOut" }
+        );
+      gsap.to(".page-two", { opacity: 0, y: 300, duration: 0.5, ease: "power2.inOut" });
+      pageTwo = false;
+    } else {
+      gsap.to(".vertical-bar > div", { y: -75, duration: 0.5, ease: "power2.inOut" });
+      gsap.to(".page-one", { y: -100, x:0 ,opacity: 0, duration: 0.5, ease: "power2.inOut" });
+      gsap.fromTo(".page-two", 
+        { opacity: 0, y: 400, x:0, duration: 0.9, ease: "power2.inOut" },
+        { opacity: 1, y: 0, x:0, duration: 0.9, ease: "power2.inOut"}
+      );
+      pageTwo = true;
+    }
+  }
+
+  verticalBar.addEventListener("click", scrollHeroDesktop);
+  return () => {
+    console.log("Desktop listener removed");
+    verticalBar.removeEventListener("click", scrollHeroDesktop);
+  };
+});
+
+mm.add("(max-width: 639px)", () => {
+  console.log("Mobile listener added");
+  document.querySelector(".page-two").style.removeProperty("transform");
+  document.querySelector(".page-one").style.removeProperty("transform");
+
+  const verticalBar = document.querySelector(".vertical-bar");
+
+  function scrollHeroMobile() {
+    if (pageTwo) {
+      gsap.to(".vertical-bar > div", { x:0, y:0, duration: 0.5, ease: "power2.inOut" });
+      gsap.fromTo(".page-one", 
+        { x: -400, y:0, opacity: 0},
+        { x: 0, y:0, opacity: 1, duration: 0.9, ease: "power2.inOut"}
+      );
+      gsap.to(".page-two", { opacity: 0, x: 400, y:0, duration: 0.5, ease: "power2.inOut" });
+      pageTwo = false;
+    } else {
+      gsap.to(".vertical-bar > div", { x:0, y:-75, duration: 0.5, ease: "power2.inOut" });
+      gsap.to(".page-one", { x: -400, y:0, opacity: 0, duration: 0.5, ease: "power2.inOut" });
+      gsap.fromTo(".page-two", 
+        { opacity: 0, x: 400, y:0},
+        { opacity: 1, x: 0, y:0, duration: 0.9, ease: "power2.inOut"}
+      );
+      pageTwo = true;
+    }
+  }
+
+  verticalBar.addEventListener("click", scrollHeroMobile);
+
+  return () => {
+    console.log("Mobile listener removed");
+    verticalBar.removeEventListener("click", scrollHeroMobile);
+  };
+});
+
+
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const ua = navigator.userAgent.toLowerCase();
@@ -88,10 +169,10 @@ document.addEventListener("DOMContentLoaded", () => {
     banner.style.color = "#000";
     banner.style.padding = "10px 20px";
     banner.style.borderRadius = "8px";
-    banner.style.fontSize = "14px";
+    banner.style.fontSize = "12px";
     banner.style.width = "100%"
     banner.style.zIndex = "9999";
-    banner.style.boxShadow = "0 4px 8px rgba(0,0,0,0.2)";
+    banner.style.boxShadow = "0 4px 8px rgba(71, 71, 71, 0.2)";
     document.body.appendChild(banner);
   }
 });
